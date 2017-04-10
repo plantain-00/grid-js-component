65e239a44028354bb161dc84d3ba2a625515418e
york yao <yaoao12306@outlook.com>
2017-04-10 08:11:17 +0800

    more datas

--- demo/common.ts
+++ demo/common.ts
@@ -18,7 +18,7 @@ export const data: GridData = {
                     value: "John",
                 },
                 {
-                    value: 50,
+                    value: 30,
                     component: "proficiency-percent",
                 },
             ],
@@ -30,6 +30,50 @@ export const data: GridData = {
                 },
                 {
                     value: 90,
+                    component: "proficiency-percent",
+                },
+            ],
+        },
+        {
+            cells: [
+                {
+                    value: "Lili",
+                },
+                {
+                    value: 10,
+                    component: "proficiency-percent",
+                },
+            ],
+        },
+        {
+            cells: [
+                {
+                    value: "Smith",
+                },
+                {
+                    value: 70,
+                    component: "proficiency-percent",
+                },
+            ],
+        },
+        {
+            cells: [
+                {
+                    value: "Lucy",
+                },
+                {
+                    value: 60,
+                    component: "proficiency-percent",
+                },
+            ],
+        },
+        {
+            cells: [
+                {
+                    value: "Emily",
+                },
+                {
+                    value: 20,
                     component: "proficiency-percent",
                 },
             ],
--- demo/vue/index.ts
+++ demo/vue/index.ts
@@ -10,7 +10,8 @@ Vue.component("proficiency-percent", {
         getStyle(this: any) {
             return {
                 width: this.data + "%",
-                backgroundColor: "green",
+                backgroundColor: this.data >= 50 ? "rgb(0, 160, 0)" : "rgb(255, 153, 0)",
+                padding: "3px",
             };
         },
     },
