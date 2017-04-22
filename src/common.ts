00cfefac5685334ef09d928afc7e68fd61d2ca42
york yao <yaoao12306@outlook.com>
2017-04-22 16:50:14 +0800

    better scroll

--- src/common.ts
+++ src/common.ts
@@ -44,15 +44,44 @@ import * as Ps from "perfect-scrollbar";
 
 export { Ps };
 
+function getDeltaFromEvent(e: WheelEvent) {
+    let deltaX = e.deltaX;
+    let deltaY = -1 * e.deltaY;
+
+    if (typeof deltaX === "undefined" || typeof deltaY === "undefined") {
+        // OS X Safari
+        deltaX = -1 * e.wheelDeltaX / 6;
+        deltaY = e.wheelDeltaY / 6;
+    }
+
+    if (e.deltaMode && e.deltaMode === 1) {
+        // Firefox in deltaMode 1: Line scrolling
+        deltaX *= 10;
+        deltaY *= 10;
+    }
+
+    if (isNaN(deltaX) && isNaN(deltaY)) {
+        // IE in some mouse drivers
+        deltaX = 0;
+        deltaY = e.wheelDelta;
+    }
+
+    if (e.shiftKey) {
+        // reverse axis with shift key
+        return { deltaX: -deltaY, deltaY: -deltaX };
+    }
+    return { deltaX, deltaY };
+}
+
 export function updateVerticalScroll(e: WheelEvent, container: HTMLElement) {
-    const distance = e.wheelDelta > 0 ? 2 : (e.wheelDelta < 0 ? -2 : 0);
-    container.scrollTop -= distance;
+    const { deltaY } = getDeltaFromEvent(e);
+    container.scrollTop -= deltaY;
     Ps.update(container);
 }
 
 export function updateHorizontalScroll(e: WheelEvent, container: HTMLElement) {
-    const distance = e.wheelDelta > 0 ? 2 : (e.wheelDelta < 0 ? -2 : 0);
-    container.scrollLeft -= distance;
+    const { deltaX } = getDeltaFromEvent(e);
+    container.scrollLeft += deltaX;
     Ps.update(container);
 }
 
