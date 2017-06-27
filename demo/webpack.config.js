8ec2a8d9e4fc96a7a3a56caf51d9d0307c7b61bb
york yao <yaoao12306@outlook.com>
2017-06-28 07:39:56 +0800

    better web pack config

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,34 +1,46 @@
 const webpack = require("webpack");
 const path = require("path");
 
-module.exports = {
-    entry: {
-        vue: "./demo/vue/index",
-        react: "./demo/react/index",
-    },
-    output: {
-        path: __dirname,
-        filename: "[name]/index.bundle.js"
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                "NODE_ENV": JSON.stringify("production")
-            }
-        }),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-            },
-            output: {
-                comments: false,
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            "vue$": "vue/dist/vue.min.js"
+const plugins = [
+    new webpack.DefinePlugin({
+        "process.env": {
+            "NODE_ENV": JSON.stringify("production")
         }
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false,
+        },
+        output: {
+            comments: false,
+        },
+    }),
+];
+
+const resolve = {
+    alias: {
+        "vue$": "vue/dist/vue.min.js"
     }
 };
+
+module.exports = [
+    {
+        entry: "./demo/vue/index",
+        output: {
+            path: path.resolve(__dirname, "vue"),
+            filename: "index.bundle.js"
+        },
+        plugins,
+        resolve,
+    },
+    {
+        entry: "./demo/react/index",
+        output: {
+            path: path.resolve(__dirname, "react"),
+            filename: "index.bundle.js"
+        },
+        plugins,
+        resolve,
+    }
+];
