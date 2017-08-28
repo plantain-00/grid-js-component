4b0ebf690727e5e4b07c95b5d3a2176b234fe283
york yao <yaoao12306@outlook.com>
2017-08-28 20:39:33 +0800

    better screenshot

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -31,6 +31,7 @@ module.exports = {
       server.listen(8000)
       const browser = await puppeteer.launch()
       const page = await browser.newPage()
+      await page.emulate({ viewport: { width: 1440, height: 900 }, userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36' })
       for (const type of ['vue', 'react']) {
         await page.goto(`http://localhost:8000/demo/${type}`)
         await page.screenshot({ path: `demo/${type}/screenshot.png`, fullPage: true })
